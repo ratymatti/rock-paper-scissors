db1d60707155ea4b6643e97449d1b90c53222820
Matti Räty <mattiraty@hotmail.com>
2023-02-23 15:32:01 +0200

    created Options component

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import { FaHandRock, FaHandPaper, FaHandScissors } from 'react-icons/fa';
 
 class Options extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerChoice: ''
+        }
+
+        this.setPlayerChoice = this.setPlayerChoice.bind(this);
+    }
+
+    setPlayerChoice(choice) {
+        this.setState({ playerChoice: choice });
+    }
+
     render() {
         return (
             <div>
-              <button><FaHandRock size='2rem'/>  </button>  
-              <button><FaHandPaper size='2rem'/>  </button> 
-              <button><FaHandScissors size='2rem'/>  </button> 
+                <button onClick={() => this.setPlayerChoice('rock')}>
+                    <FaHandRock size='2rem'/>
+                </button>  
+                <button onClick={() => this.setPlayerChoice('paper')}>
+                    <FaHandPaper size='2rem'/>
+                </button> 
+                <button onClick={() => this.setPlayerChoice('scissors')}>
+                    <FaHandScissors size='2rem'/>
+                </button> 
             </div>
         );
     }
 }
 
 export default Options;
+
