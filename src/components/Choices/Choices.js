f112b62362c319914abd9927ab1960f931a17a43
Matti Räty <mattiraty@hotmail.com>
2023-03-02 14:53:02 +0200

    refactored Choices -component

--- src/components/Choices/Choices.js
+++ src/components/Choices/Choices.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, useMemo } from 'react';
 import { FaHandRock, FaHandPaper, FaHandScissors, FaSpinner } from 'react-icons/fa';
 import './Choices.css';
+import memoizeOne from 'memoize-one';
+
+const iconMap = {
+  rock: FaHandRock,
+  paper: FaHandPaper,
+  scissors: FaHandScissors,
+};
 
 class Choices extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.renderChoice = this.renderChoice.bind(this);
+    this.memoizedRenderChoice = memoizeOne(this.renderChoice);
+  }
 
-        this.renderChoice = this.renderChoice.bind(this);
-    }
+  renderChoice(choice) {
+    const Icon = iconMap[choice];
+    return Icon ? <Icon style={{ fontSize: '3rem' }} /> : null;
+  }
 
-    renderChoice(choice) {
-        switch(choice) {
-            case 'rock':
-                return <FaHandRock size='3rem'/>;
-            case 'paper':
-                return <FaHandPaper size='3rem'/>;
-            case 'scissors':
-                return <FaHandScissors size='3rem'/>;
-            default:
-                return null;
-        }
-    }
+  render() {
+    const { playersChoice, computersChoice, weHaveAWinner } = this.props;
 
-    render() {
-        const renderChoice = this.renderChoice;
-
-        return (
-            <div className='choices'>
-                <div className='player'>
-                    <div className='icon'>
-                        {this.props.playersChoice && renderChoice(this.props.playersChoice)}
-                    </div>
-                    <h3>Players Choice</h3>    
-                </div>
-                <div className='computer'>
-                    <div className='spinningIcon'>
-                        {this.props.computersChoice && !this.props.weHaveAWinner && <FaSpinner size='3rem'/>}
-                    </div>
-                    <div className='icon'>
-                        {this.props.computersChoice && this.props.weHaveAWinner && this.renderChoice(this.props.computersChoice)}
-                    </div>
-                    <h3>Computers Choice</h3>
-                </div>   
-            </div>
-        );
-    }
+    return (
+      <div className='choices'>
+        <div className='player'>
+          <div className='icon'>
+            {playersChoice && this.memoizedRenderChoice(playersChoice)}
+          </div>
+          <h3>Players Choice</h3>
+        </div>
+        <div className='computer'>
+          <div className='spinningIcon'>
+            {computersChoice && !weHaveAWinner && <FaSpinner size='3rem' />}
+          </div>
+          <div className='icon'>
+            {computersChoice && weHaveAWinner && this.memoizedRenderChoice(computersChoice)}
+          </div>
+          <h3>Computers Choice</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Choices;
